a285aa40db06d2b29d88239545771a929e262d14
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-06-03 14:04:27 -0300

    Agregando bootstrap.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import("vendor/bootstrap.css");
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
