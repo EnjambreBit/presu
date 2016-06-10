8d18724f9655f43fc7cda01cc94e0ed41948af0c
Iván Hoffmann <digital.h@gmail.com>
2016-06-10 15:41:39 -0300

    Total, Total mas ganancia e IVA en presupuesto

--- app/controllers/presupuesto.js
+++ app/controllers/presupuesto.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  iva: true,
   precioTotal: Ember.computed('model.items.@each.subtotal', function() {
     let total = 0;
     let items = this.get("model.items");
@@ -11,4 +12,36 @@ export default Ember.Controller.extend({
 
     return total;
   }),
+  precioMasGanancia: Ember.computed('ganancia','precioTotal', function() {
+    let ganancia = 0;
+    ganancia = this.get("ganancia");
+    if (isNaN(ganancia)) {
+      ganancia = 50;
+      this.set("ganancia", ganancia);
+    }
+    var gan = ganancia / 100;
+    var precioTotal = this.get("precioTotal");
+    var importe_ganancia = precioTotal * gan;
+    this.set("importe_ganancia", importe_ganancia);
+    var total = precioTotal * (1 + gan);
+
+    return total;
+  }),
+  precioFinal: Ember.computed('precioMasGanancia','iva', function() {
+    var precioMasGanancia = this.get("precioMasGanancia");
+    var total = precioMasGanancia;
+    var iva = this.get("iva");
+
+    // Si se aplica IVA
+		if (iva===true) {
+			total = precioMasGanancia * 1.21;
+      var importe_iva = precioMasGanancia * 0.21;
+      this.set("importe_iva", importe_iva);
+		} else {
+			total = precioMasGanancia;
+		}
+
+    return total;
+  }),
+
 });
