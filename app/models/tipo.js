0732b5fbff285fb4a4739b35f24634cd6e35523d
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-06-11 02:26:42 -0300

    Corrigiendo el bug que a veces hacía que el subtotal aparezca en 0 y el texto NaN en algunos campos relacionados.
    El problema se ocasionaba porque las relaciones de item y tipo estaban
    mal generadas en una de las direcciones, entonces un registro tipo
    solo podía estar asociada a un solo item, cuando en realidad la relación
    debería ser de uno a muchos.

--- app/models/tipo.js
+++ app/models/tipo.js
@@ -1,10 +1,10 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   descripcion: attr('string'),
   precioPorHora: attr('number'),
 
-  item: belongsTo('item', {async: true})
+  item: hasMany('item', {async: true})
 });
