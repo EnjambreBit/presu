e26f198799d44049041f7d309f4eed1f27c79146
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-06-11 02:11:46 -0300

    Corrigiendo el bug que ocasionaba alertas de desuso en la consola del navegador.

--- app/controllers/presupuesto.js
+++ app/controllers/presupuesto.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  ganancia: 50,
   iva: true,
   precioTotal: Ember.computed('model.items.@each.subtotal', function() {
     let total = 0;
@@ -12,36 +13,29 @@ export default Ember.Controller.extend({
 
     return total;
   }),
-  precioMasGanancia: Ember.computed('ganancia','precioTotal', function() {
-    let ganancia = 0;
-    ganancia = this.get("ganancia");
-    if (isNaN(ganancia)) {
-      ganancia = 50;
-      this.set("ganancia", ganancia);
-    }
-    var gan = ganancia / 100;
+  precioMasGanancia: Ember.computed('ganancia', 'precioTotal', function() {
+    let ganancia = this.get("ganancia");
+    var gan = ganancia / 100.0;
     var precioTotal = this.get("precioTotal");
-    var importe_ganancia = precioTotal * gan;
-    this.set("importe_ganancia", importe_ganancia);
     var total = precioTotal * (1 + gan);
 
     return total;
   }),
-  precioFinal: Ember.computed('precioMasGanancia','iva', function() {
-    var precioMasGanancia = this.get("precioMasGanancia");
-    var total = precioMasGanancia;
-    var iva = this.get("iva");
 
-    // Si se aplica IVA
-		if (iva===true) {
-			total = precioMasGanancia * 1.21;
-      var importe_iva = precioMasGanancia * 0.21;
-      this.set("importe_iva", importe_iva);
-		} else {
-			total = precioMasGanancia;
-		}
-
-    return total;
+  importe_ganancia: Ember.computed("precioTotal", "ganancia", function() {
+    return this.get("precioTotal") * this.get("ganancia");
   }),
+
+  precioFinal: Ember.computed('precioMasGanancia', 'iva', 'importe_iva', function() {
+    if (this.get("iva")) {
+      return this.get("precioMasGanancia") + this.get("importe_iva");
+    } else {
+      return this.get("precioMasGanancia");
+    }
+  }),
+
+  importe_iva: Ember.computed("precioMasGanancia", function() {
+    return this.get("precioMasGanancia") * 0.21;
+  })
 
 });
